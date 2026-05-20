8edc8d0be42acbcd877d019eb7f40132f3cead1c
Aashutosh Dhungana <dhunganayt1205@gmail.com>
2026-05-20 11:20:03 +0000

    Added google analytics

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: "G-0XHTP7MGYB",
     },
     locale: "en-US",
     baseUrl: "notes.aashutoshdhungana.com.np",
